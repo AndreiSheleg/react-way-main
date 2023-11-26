3c87058416578896c2774bf8e4a97a02e28dd463
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-11-26 23:53:38 +0300

    upgrade Storybook-Stories for Select

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -6,3 +6,27 @@ export default {
     component: Select,
 };
 
+export const BaseSelectExampleWithValue = () => {
+    return (
+        <>
+            <Select value={'3'} onChange={action('Value changed!')} items={[
+                {value: '1', title: 'Minsk'},
+                {value: '2', title: 'Vitebsk'},
+                {value: '3', title: 'Gomel'},
+            ]}/>
+        </>
+    )
+}
+
+export const WithoutValue = () => {
+    return (
+        <>
+            <Select onChange={action('Value changed!')} items={[
+                {value: '1', title: 'Minsk'},
+                {value: '2', title: 'Vitebsk'},
+                {value: '3', title: 'Gomel'},
+            ]}/>
+        </>
+    )
+}
+
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -6,17 +6,17 @@ type ItemType = {
 }
 
 type SelectPropsType = {
-    value: any
+    value?: any
     onChange: (value: any) => void
     items: ItemType[]
 }
 
 export const Select = (props: SelectPropsType) => {
-
+const selectedItem = props.items.find(el => el.value === props.value)
     return (
         <div>
-            <div>{}</div>
-            {props.items.map(item => <div>{item.title}</div>)}
+            <h3>{selectedItem && selectedItem.title}</h3>
+            {props.items.map(item => <div key={item.value}>{item.title}</div>)}
         </div>
     )
 };
