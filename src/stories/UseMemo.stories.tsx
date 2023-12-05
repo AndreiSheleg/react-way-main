ed13aa41f41dba45b52cfd27ef92e59f1d9fdd28
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-12-06 00:01:45 +0300

    add useMemo stories

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,79 @@
+import React, {useMemo, useState} from "react";
+
+export default {
+    title: 'useMemo',
+}
+
+export const DifficultCountingExample = () => {
+    const [x, setX] = useState<number>(5)
+    const [y, setY] = useState<number>(5)
+
+    let resultX = 1
+    let resultY = 1
+
+    resultX = useMemo(() => {
+        let tempResultX = 1
+        for (let i = 1; i <= x; i++) {
+            let fake = 0
+            while (fake < 100000) {
+                fake++
+                const fakeRandom = Math.random()
+            }
+            tempResultX = tempResultX * i
+        }
+        return tempResultX
+    }, [x]);
+
+
+    for (let i = 1; i <= y; i++) {
+        resultY = resultY * i
+    }
+
+    return <>
+        <input value={x} onChange={(event) => setX(parseInt(event.currentTarget.value, 10))}/>
+        <input value={y} onChange={(event) => setY(parseInt(event.currentTarget.value, 10))}/>
+        <hr/>
+        <div>
+            Result for x: {resultX}
+        </div>
+        <div>
+            Result for y: {resultY}
+        </div>
+    </>
+}
+
+
+const UsersSecret = (props: { users: string[] }) => {
+    console.log('вызов компоненты UsersSecret: ')
+    return <div>{
+        props.users.map((el, index) => <div key={index}>{el}</div>)
+    }</div>
+}
+const Users = React.memo(UsersSecret)
+
+export const HelpsToReactMemo = () => {
+
+    console.log('вызов компоненты HelpsToReactMemo: ')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dim', 'Valera', 'Artem'])
+
+    const newArray = useMemo(() => {
+        return users.filter(u => u.toLowerCase().indexOf('a') > -1)
+    }, [users])
+
+    const addUser = () => {
+        const newUser = 'Katya ' + new Date().getTime()
+        setUsers([newUser, ...users])
+    }
+
+    return <>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
+        <button onClick={() => addUser() }>addUser</button>
+        {counter}
+        <Users users={newArray}/>
+    </>
+
+}
